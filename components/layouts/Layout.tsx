5b3d73963b9af4ed834b472ca9a9a7438ae73906
Pablo Rodriguez <pablorodriguez88@gmail.com>
2022-11-30 21:48:51 -0300

    static page 151 pokemon

---
+++ components/layouts/Layout.tsx
@@ -0,0 +1,31 @@
+import { FC, PropsWithChildren } from "react";
+import Head from "next/head";
+import { Navbar } from "../ui";
+
+interface LayoutProps extends PropsWithChildren {
+  title?: string;
+}
+
+export const Layout: FC<LayoutProps> = ({ children, title }) => {
+  return (
+    <>
+      <Head>
+        <title>{title || "Pokemon App"}</title>
+        <meta name="author" content="Pablo Rodriguez" />
+        <meta
+          name="description"
+          content={`Informacion sobre el pokemon ${title}`}
+        />
+        <meta name="keywords" content={`${title}, pokemon, pokedex`} />
+      </Head>
+      <Navbar />
+      <main
+        style={{
+          padding: "0px 20px",
+        }}
+      >
+        {children}
+      </main>
+    </>
+  );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,49 @@
-import { Button } from "@nextui-org/react";
+import { Layout } from "../components/layouts";
+import { GetStaticProps, NextPage } from "next";
+import { pokeApi } from "../api";
+import { PokemonListResponse, SmallPokemon } from "../interfaces";
+import { Grid } from "@nextui-org/react";
+import { PokemonCard } from "../components/pokemon";
 
-export default function HomePage() {
-  return (
-    <>
-      <Button color={"gradient"}>Hola Mundo</Button>
-    </>
-  );
+interface Props {
+  pokemons: SmallPokemon[];
 }
+
+const HomePage: NextPage<Props> = ({ pokemons }) => {
+  return (
+    <Layout title="Listado de Pokemons">
+      <Grid.Container gap={2} justify="flex-start">
+        {pokemons.map((pokemon) => (
+          <PokemonCard pokemon={pokemon} key={pokemon.id} />
+        ))}
+      </Grid.Container>
+    </Layout>
+  );
+};
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  const { data } = await pokeApi.get<PokemonListResponse>(
+    "/pokemon?limit=151",
+    {
+      headers: {
+        "accept-encoding": "*",
+      },
+    }
+  );
+
+  const pokemons = data.results.map((poke, i) => ({
+    ...poke,
+    id: i + 1,
+    img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${
+      i + 1
+    }.svg`,
+  }));
+
+  return {
+    props: {
+      pokemons,
+    },
+  };
+};
+
+export default HomePage;
