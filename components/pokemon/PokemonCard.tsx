b98339e85ffed832cd84619dde4c3fa4678e3c90
Pablo Rodriguez <pablorodriguez88@gmail.com>
2022-12-07 20:07:03 -0300

    rutes for every pokemon, static generated

--- components/pokemon/PokemonCard.tsx
+++ components/pokemon/PokemonCard.tsx
@@ -1,4 +1,5 @@
 import { Card, Grid, Row, Text } from "@nextui-org/react";
+import { useRouter } from "next/router";
 import { FC } from "react";
 import { SmallPokemon } from "../../interfaces";
 
@@ -7,10 +8,15 @@ interface Props {
 }
 
 export const PokemonCard: FC<Props> = ({ pokemon }) => {
+  const router = useRouter();
+
+  const onClick = () => {
+    router.push(`/pokemon/${pokemon.id}`);
+  };
   const { id, name, img } = pokemon;
   return (
-    <Grid xs={6} sm={3} md={2} xl={12} key={id}>
-      <Card isHoverable isPressable>
+    <Grid xs={12} sm={6} md={3} lg={2} xl={1} key={id}>
+      <Card isHoverable isPressable onClick={onClick}>
         <Card.Body css={{ padding: 1 }}>
           <Card.Image src={img} width="100%" height={140} />
         </Card.Body>
